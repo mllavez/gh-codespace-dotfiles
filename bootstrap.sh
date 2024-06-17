b6d914803cdc5032040cc1a37bd9077a5ceb3086
Marcos Alexander Chávez <marcos@llavez.com>
2024-06-17 15:17:48 -0700

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,7 +14,7 @@ ln -s ~/.local/squashfs-root/AppRun ~/.local/bin/nvim
 
 # Bring in our custom neovim config
 echo "XDG_CONFIG_HOME=$HOME" >> ~/.profile
-git clone https://github.com/shea-parkes/neovim-config ~/.config/nvim
+git clone https://github.com/mllavez/nvim ~/.config/nvim
 cd ~/.config/nvim
 git submodule init
 git submodule update
@@ -23,12 +23,6 @@ git submodule update
 cd /workspaces/$RepositoryName
 # poetry run pip install pynvim ipython  # Avoid if we can help it
 poetry run nvim --headless +":UpdateRemotePlugins" +"q!"
-
-# Go ahead and configure vim as well while we're at it
-git clone https://github.com/shea-parkes/vim-config ~/.vim
-cd ~/.vim
-git submodule init
-git submodule update
 
 # Setup git completions for bash
 curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
